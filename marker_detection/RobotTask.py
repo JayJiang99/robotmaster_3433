7dfa6a015adcee93e2468200439186a4782a2b15
zhiyi jiang <zyjiang97@outlook.com>
2024-01-22 16:46:49 +0800

    Update RobotTask.py

--- marker_detection/RobotTask.py
+++ marker_detection/RobotTask.py
@@ -1,8 +1,12 @@
 from robomaster import robot
+from robomaster import led
+from robomaster import gimbal
+import cv2
 import time
+
 from Detector import MarkerDetector
 from RobotMove import RobotMover
-import cv2
+
 
 class RobotTask:
     def __init__(self):
@@ -11,17 +15,22 @@ class RobotTask:
         self.ep_vision = self.ep_robot.vision
         self.ep_chassis = self.ep_robot.chassis
         self.ep_camera = self.ep_robot.camera
+        # Set travel mode to 'chassis lead'
+        self.ep_robot.gimbal.recenter().wait_for_completed()
+        self.ep_robot.gimbal.set_follow_chassis(True)
         
         self.detector = MarkerDetector(self.ep_vision)
         self.mover = RobotMover(self.ep_chassis)
 
-    def run_task_rotate_marker_detection(self):
+    def run_task2_rotate_marker_detection(self):
+        print('start')
         self.ep_camera.start_video_stream(display=False)
         self.mover.rotate_w_speed(z_value = 10,duration=15)
         start_time = time.time()
         while time.time() - start_time < 15:
             img = self.ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
             img_with_markers = self.detector.draw_markers(img)
+            self.ep_robot.play_sound(filename="hit").wait_for_completed()
             cv2.imshow("Markers", img_with_markers)
             cv2.waitKey(1)
         cv2.destroyAllWindows()
@@ -33,6 +42,6 @@ class RobotTask:
 if __name__ == '__main__':
     task = RobotTask()
     try:
-        task.run_task_rotate_marker_detection()
+        task.run_task2_rotate_marker_detection()
     finally:
         task.close()
